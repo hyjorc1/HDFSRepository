542776fcbe65e8ece1f791001a7830aeeff380ab
Yijia Huang <hyjorc1@gmail.com>
2019-05-22 17:57:54 -0500

    update

--- src/Serialization/MyFile.java
+++ src/Serialization/MyFile.java
@@ -19,7 +19,7 @@ public class MyFile extends java.io.File {
 	}
 
 	/* -- Path-component accessors -- */
-	
+
 	@Override
 	public String getName() {
 		String name = node.getName();
@@ -68,7 +68,7 @@ public class MyFile extends java.io.File {
 	public MyFile getCanonicalFile() {
 		return this;
 	}
-	
+
 	/* -- Attribute accessors -- */
 
 	@Override
@@ -112,26 +112,26 @@ public class MyFile extends java.io.File {
 	}
 
 	/* -- File operations -- */
-	
+
 	@Override
 	public boolean createNewFile() throws IOException {
-        return false;
-    }
-	
+		return false;
+	}
+
 	@Override
 	public boolean delete() {
 		if (node.getParent() != null && node.getIdx() != -1) {
 			node.getParent().getList().remove(node.getIdx());
 			return true;
 		}
-        return false;
-    }
-	
+		return false;
+	}
+
 	@Override
 	public void deleteOnExit() {
-		
+
 	}
-	
+
 	@Override
 	public String[] list() {
 		List<FileNode> list = node.getList();
@@ -142,56 +142,135 @@ public class MyFile extends java.io.File {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public String[] list(FilenameFilter filter) {
-        String names[] = list();
-        if ((names == null) || (filter == null)) {
-            return names;
-        }
-        List<String> v = new ArrayList<>();
-        for (int i = 0 ; i < names.length ; i++) {
-            if (filter.accept(this, names[i])) {
-                v.add(names[i]);
-            }
-        }
-        return v.toArray(new String[v.size()]);
-    }
-	
+		String names[] = list();
+		if ((names == null) || (filter == null)) {
+			return names;
+		}
+		List<String> v = new ArrayList<>();
+		for (int i = 0; i < names.length; i++) {
+			if (filter.accept(this, names[i])) {
+				v.add(names[i]);
+			}
+		}
+		return v.toArray(new String[v.size()]);
+	}
+
 	@Override
 	public MyFile[] listFiles() {
 		List<FileNode> list = node.getList();
-		if (list == null) return null;
-        int n = list.size();
-        MyFile[] fs = new MyFile[n];
-        for (int i = 0; i < n; i++) {
-            fs[i] = new MyFile(list.get(i));
-        }
-        return fs;
-    }
-	
+		if (list == null)
+			return null;
+		int n = list.size();
+		MyFile[] fs = new MyFile[n];
+		for (int i = 0; i < n; i++) {
+			fs[i] = new MyFile(list.get(i));
+		}
+		return fs;
+	}
+
 	@Override
 	public MyFile[] listFiles(FilenameFilter filter) {
 		List<FileNode> list = node.getList();
-		if (list == null) return null;
-        ArrayList<File> files = new ArrayList<>();
-        for (FileNode n : list)
-            if ((filter == null) || filter.accept(this, n.getName()))
-                files.add(new MyFile(n));
-        return files.toArray(new MyFile[files.size()]);
-    }
-	
+		if (list == null)
+			return null;
+		ArrayList<File> files = new ArrayList<>();
+		for (FileNode n : list)
+			if ((filter == null) || filter.accept(this, n.getName()))
+				files.add(new MyFile(n));
+		return files.toArray(new MyFile[files.size()]);
+	}
+
 	@Override
 	public MyFile[] listFiles(FileFilter filter) {
 		List<FileNode> list = node.getList();
-		if (list == null) return null;
-        ArrayList<MyFile> files = new ArrayList<>();
-        for (FileNode n : list) {
-        	MyFile f = new MyFile(n);
-            if ((filter == null) || filter.accept(f))
-                files.add(f);
+		if (list == null)
+			return null;
+		ArrayList<MyFile> files = new ArrayList<>();
+		for (FileNode n : list) {
+			MyFile f = new MyFile(n);
+			if ((filter == null) || filter.accept(f))
+				files.add(f);
+		}
+		return files.toArray(new MyFile[files.size()]);
+	}
+
+	@Override
+	public boolean mkdir() {
+		return true;
+	}
+
+	@Override
+	public boolean mkdirs() {
+		return true;
+	}
+
+	@Override
+	public boolean renameTo(File dest) {
+		return true;
+	}
+	
+	@Override
+	public boolean setLastModified(long time) {
+		return true;
+	}
+	
+	@Override
+	public boolean setReadOnly() {
+		return true;
+	}
+	
+	@Override
+	public boolean setWritable(boolean writable, boolean ownerOnly) {
+		return true;
+	}
+	
+	@Override
+	public boolean setWritable(boolean writable) {
+        return true;
+    }
+	
+	@Override
+	public boolean setReadable(boolean readable, boolean ownerOnly) {
+        return true;
+    }
+	
+	@Override
+	public boolean setReadable(boolean readable) {
+		return true;
+    }
+	
+	@Override
+	public boolean setExecutable(boolean executable, boolean ownerOnly) {
+        return true;
+    }
+	
+	@Override
+	public boolean setExecutable(boolean executable) {
+        return true;
+    }
+	
+	@Override
+	public boolean canExecute() {
+        return false;
+    }
+	
+	/* -- Filesystem interface -- */
+	/* -- Disk usage -- */
+	/* -- Temporary files -- */
+	/* -- Basic infrastructure -- */
+	public int compareTo(MyFile file) {
+        return node.getName().compareTo(file.node.getName());
+    }
+	
+	@Override
+	public boolean equals(Object obj) {
+        if ((obj != null) && (obj instanceof MyFile)) {
+            return compareTo((MyFile)obj) == 0;
         }
-        return files.toArray(new MyFile[files.size()]);
+        return false;
     }
 	
 }
